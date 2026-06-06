3dbc6ebfd61f6362ef910c01192d9eeb43d8c46a
antopaulev <antopaul2022@gmail.com>
2026-06-06 11:25:48 +0530

    optimal solution for finding second largest element

---
+++ Array/Eassy/second_largest.py
@@ -0,0 +1,14 @@
+def second_largest(arr):
+    largest = float('-inf')
+    second_largest = float('-inf')
+    n = len(arr)
+    for i in range (0, n):
+        if arr[i] > largest:
+            second_largest = largest
+            largest = arr[i]
+        elif arr[i] > second_largest and arr[i] != largest:
+            second_largest = arr[i]
+    return second_largest   
+
+arr = [1, 2, 3, 4, 5]
+print("Second largest element in the array is:", second_largest(arr))
