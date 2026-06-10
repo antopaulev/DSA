8c6dad8e9defd3ae9dd90cead08af1555d6a59f6
antopaulev <antopaul2022@gmail.com>
2026-06-10 12:23:51 +0530

    right rotate by k values

---
+++ Array/Eassy/right_rotate_by_k.py
@@ -0,0 +1,14 @@
+def right_rotate_by_k(arr, k):  
+    n = len(arr)
+    k = k % n  # Handle cases where k is greater than n
+    while k > 0:
+        last_element = arr[n-1]
+        for i in range (n-2, -1, -1):
+            arr[i+1] = arr[i]
+        arr[0] = last_element
+        k -= 1
+    return arr  
+
+arr = [1, 2, 3, 4, 5]
+k = 3
+print(right_rotate_by_k(arr, k))
