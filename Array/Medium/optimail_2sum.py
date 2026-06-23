4a926045918a2d7f38711d87ef02022a7735ee67
antopaulev <antopaul2022@gmail.com>
2026-06-23 17:31:24 +0530

    optimail solution for two sum problem

---
+++ Array/Medium/optimail_2sum.py
@@ -0,0 +1,12 @@
+def find_2sum(nums, target):
+    n = len(nums)
+    hash_map = {}
+    for i in range (0 , n):
+        remaining = target - nums[i] 
+        if remaining in hash_map:
+            return [hash_map[remaining] , i]
+        hash_map[nums[i]] = i
+ 
+nums = [5,9,1,2,4,15,6,3]
+target = 13 
+print(find_2sum(nums, target))
